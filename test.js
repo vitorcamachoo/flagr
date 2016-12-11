2ffe780980f6dd35eace0ecaab303cec730c638b
Vitor Camacho <vitor.camacho@exictos.com>
2016-12-11 18:43:11 +0000

    Add some test with no flag prefix (--)

--- test.js
+++ test.js
@@ -5,7 +5,10 @@ const { get, exist } = require('./index');
 test(t => {
   t.true(exist('--prod', ['webpack', '--prod']), 'Has --prod flag');
   t.true(exist('--prod', ['webpack', '--prod="true"']), 'Has --prod flag');
+  t.true(exist('prod', ['webpack', '--prod']), 'Has --prod flag');
   t.false(exist('--prod', ['webpack', '--test']), '--prod not found');
   t.is(get('--env', ['webpack', '--env="production"', '-w', '-w="teste/**/.js"']), "production", 'Get flag content');
+  t.is(get('env', ['webpack', '--env="production"', '-w', '-w="teste/**/.js"']), "production", 'Get flag content');
   t.is(get('--env', ['webpack', '--env=""']), '', 'Empty content');
+  t.not(get('--env', ['webpack', '--env=""']), undefined, 'Empty content');
 });
